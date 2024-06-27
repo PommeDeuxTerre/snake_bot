696f36cbab675ecf3d57d9c64e9a3406e5899f28
PommeDeuxTerre <147161646+PommeDeuxTerre@users.noreply.github.com>
2024-06-27 10:12:43 +0200

    update datas struct

--- datas.py
+++ datas.py
@@ -1,2 +1,9 @@
+# different bots
+POMME_DES_TERRES = 0
+POMME_JEDUSOR = 1
+POMME_ELVIS_JEDUSOR = 2
+TEST = 3
+#name that will be shown in the game
 name = "PommeJedusor"
-bot = 1
+#bot that will play
+bot = POMME_ELVIS_JEDUSOR
